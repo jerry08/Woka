c0581b779785294836fde8bf9a32238da370d7dc
jerrry08 <jeremyyearwood99@gmail.com>
2023-11-04 19:24:01 -0400

    Add RefreshView workaround for iOS and Mac (Closes #6)

---
+++ Woka/Handlers/RefreshView/MauiRefreshView.macios.cs
@@ -0,0 +1,27 @@
+﻿using CoreGraphics;
+using Microsoft.Maui.Platform;
+using UIKit;
+
+namespace Woka.Handlers.RefreshView;
+
+public class WokaRefreshView : MauiRefreshView
+{
+    UIView? _contentView;
+
+    public override void AddSubview(UIView view)
+    {
+        base.AddSubview(view);
+        _contentView = view;
+    }
+
+    public override CGRect Bounds
+    {
+        get => base.Bounds;
+        set
+        {
+            base.Bounds = value;
+            if (_contentView != null)
+                _contentView.Frame = value;
+        }
+    }
+}
---
+++ Woka/Handlers/RefreshView/RefreshViewHandler.macios.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Maui.Platform;
+
+namespace Woka.Handlers;
+
+public class RefreshViewHandler : Microsoft.Maui.Handlers.RefreshViewHandler
+{
+    protected override MauiRefreshView CreatePlatformView()
+    {
+        return new MauiRefreshView();
+    }
+}
