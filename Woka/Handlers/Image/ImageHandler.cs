ee9a9526091c89f02bc56fd4b56de0227dea8fab
jerry08 <jeremyyearwood99@gmail.com>
2024-06-10 16:22:47 -0400

    Closes #7

--- Woka/Handlers/Image/ImageHandler.cs
+++ Woka/Handlers/Image/ImageHandler.cs
@@ -1,24 +1,30 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Networking;
+﻿using Microsoft.Maui.Networking;
+#if __IOS__ || MACCATALYST
+using PlatformView = UIKit.UIImageView;
+#elif ANDROID
+using PlatformView = Android.Widget.ImageView;
+#elif WINDOWS
+using PlatformView = Microsoft.UI.Xaml.Controls.Image;
+#elif TIZEN
+using PlatformView = Tizen.UIExtensions.NUI.Image;
+#elif (NETSTANDARD || !PLATFORM) || (NET6_0_OR_GREATER && !IOS && !ANDROID && !TIZEN)
+using PlatformView = System.Object;
+#endif
 
 namespace Woka.Handlers;
 
 public class ImageHandler : Microsoft.Maui.Handlers.ImageHandler
 {
-    public Image? Image { get; set; }
-
-    public override void SetVirtualView(IView view)
+    protected override void ConnectHandler(PlatformView platformView)
     {
-        base.SetVirtualView(view);
+        base.ConnectHandler(platformView);
+        Connectivity.Current.ConnectivityChanged += ConnectivityChanged;
+    }
 
-        if (VirtualView is Image image)
-        {
-            Image = image;
-
-            Connectivity.Current.ConnectivityChanged -= ConnectivityChanged;
-            Connectivity.Current.ConnectivityChanged += ConnectivityChanged;
-        }
+    protected override void DisconnectHandler(PlatformView platformView)
+    {
+        base.DisconnectHandler(platformView);
+        Connectivity.Current.ConnectivityChanged -= ConnectivityChanged;
     }
 
     private void ConnectivityChanged(object? sender, ConnectivityChangedEventArgs e)
